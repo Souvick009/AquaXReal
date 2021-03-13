6741254ec98419ed87e22423f518a39f84b88d48
Souvick009 <67007403+Souvick009@users.noreply.github.com>
2021-03-13 17:42:29 +0530

    Create queue.js

---
+++ commands/Music/queue.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const ytdl = require('ytdl-core');
+const ytSearch = require('yt-search');
+
+module.exports = {
+    name: "skip",
+    aliases: [],
+    accessableby: "Manage Messages",
+    description: "Check ping of the bot",
+    usage: ">>skip",
+    example: ">>skip",
+    cooldown: 5,
+    category: "Music",
+    run: async (bot, message, args) => {
+        if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
+
+        let queue = await bot.distube.getQueue(message);
+
+        message.channel.send('Current queue:\n' + queue.songs.map((song, id) =>
+            `**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``
+        ).slice(0, 10).join("\n"));
+    }
+
+}
