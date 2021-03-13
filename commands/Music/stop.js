a360a0619c43729607d8f21f10741ad32d1c1cf0
Souvick009 <67007403+Souvick009@users.noreply.github.com>
2021-03-13 17:06:19 +0530

    .

--- commands/Music/stop.js
+++ commands/Music/stop.js
@@ -22,22 +22,20 @@ module.exports = {
         //This is our server queue. We are getting this server queue from the global queue.
         const server_queue = queue.get(message.guild.id);
 
-        //If the server queue does not exist (which doesn't for the first video queued) then create a constructor to be added to our global queue.
-        if (!server_queue) {
+        let song = {};
 
-            const queue_constructor = {
-                voice_channel: voice_channel,
-                text_channel: message.channel,
-                connection: null,
-                songs: []
-            }
-
-            await stop_song(message, server_queue);
-            await message.channel.send('Leaving channel :smiling_face_with_tear:')
+        //If the first argument is a link. Set the song object to have two keys. Title and URl.
+        if (ytdl.validateURL(args[0])) {
+            const song_info = await ytdl.getInfo(args[0]);
+            song = { title: song_info.videoDetails.title, url: song_info.videoDetails.video_url }
         }
-    }
 
+        await stop_song(message, server_queue);
+        await message.channel.send('Leaving channel :smiling_face_with_tear:')
+    }
 }
+
+
 
 const stop_song = (message, server_queue) => {
     if (!message.member.voice.channel) return message.channel.send('You need to be in a channel to execute this command!');
