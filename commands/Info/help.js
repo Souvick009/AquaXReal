1d3a4951602fdc68de0e2fb162ac8765720e1672
Souvick009 <67007403+Souvick009@users.noreply.github.com>
2021-03-14 20:38:30 +0530

    Create help.js

---
+++ commands/Info/help.js
@@ -0,0 +1,71 @@
+const Discord = require("discord.js");
+const { stripIndents } = require("common-tags");
+
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  category: "Info",
+  description: "Returns all commands, or one specific command info",
+  usage: "<command | alias>",
+  example: "=help, =help mute, =help clear, =help avatar",
+  accessableby: "None",
+  run: async (bot, message, args) => {
+    // If there's an args found
+    // Send the info of that command found
+    // If no info found, return not found embed.
+    if (args[0]) {
+      return getCMD(bot, message, args[0]);
+    } else {
+      // Otherwise send all the commands available
+      // Without the cmd info
+      return getAll(bot, message);
+    }
+  },
+};
+
+function getAll(bot, message) {
+  const embed = new Discord.MessageEmbed().setColor("#00fff3");
+
+  // Map all the commands
+  // with the specific category
+
+  const commands = (category) => {
+    return bot.commands.filter(cmd => cmd.category === category).filter(cmd => cmd.secret === false || !cmd.secret).map(cmd => `\`${cmd.name}\``).join(" | ");
+  }
+  const categories = ["Info", "Music"]
+  // Map all the categories
+  const lines = categories.map((category, name) => "**" + category + "**" + "\n" + commands(category)
+  );
+  console.log(bot.categories)
+  return message.channel.send(embed.setDescription(`${lines.join("\n")} \n **Prefix (Default)** \n = \n For More Information Use **=help command** \n Example:- **=help kick**`).setFooter(message.author.tag, message.author.displayAvatarURL()).setThumbnail(message.guild.iconURL()).setTimestamp().setTitle("Here are the available commands you can use:"));
+}
+
+function getCMD(bot, message, input) {
+  const embed = new Discord.MessageEmbed();
+
+  // Get the cmd by the name or alias
+  const cmd =
+    bot.commands.get(input.toLowerCase()) ||
+    bot.commands.get(bot.aliases.get(input.toLowerCase()));
+
+  let info = `No information found for command **${input.toLowerCase()}**`;
+
+  // If no cmd is found, send not found embed
+  if (!cmd) {
+    return message.channel.send(embed.setColor("RED").setDescription(info));
+  }
+
+  // Add all cmd info to the embed
+  if (cmd.name) info = `**Command name**: ${cmd.name}`;
+  if (cmd.aliases)
+    info += `\n**Aliases**: ${cmd.aliases.map((a) => `\`${a}\``).join(", ")}`;
+  if (cmd.description) info += `\n**Description**: ${cmd.description}`;
+  if (cmd.example) info += `\n**Example**: ${cmd.example}`;
+  if (cmd.accessableby) info += `\n**Accessableby**: ${cmd.accessableby}`;
+  if (cmd.usage) {
+    info += `\n**Usage**: ${cmd.usage}`;
+    embed.setFooter(`Syntax: <> = required, [] = optional`);
+  }
+
+  return message.channel.send(embed.setColor("#00fff3").setDescription(info));
+}
