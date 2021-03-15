b805a2437ea2a104e528458be1a9fdb5cbb5a9d3
Souvick009 <67007403+Souvick009@users.noreply.github.com>
2021-03-15 20:51:49 +0530

    Update queue.js

--- commands/Music/queue.js
+++ commands/Music/queue.js
@@ -21,13 +21,27 @@ module.exports = {
             curqueue = queue.songs.map((song, id) =>
                 `**${id + 1}**. ***${song.name}*** - \`${song.formattedDuration}\`\nRequested by: ${song.user}`
             ).slice(0, 10).join("\n");
-            const currentqueue = new Discord.MessageEmbed();
-            currentqueue.setTitle("Current Queue!");
-            currentqueue.setDescription(curqueue);
-            currentqueue.setColor("#FFFF00");
-            currentqueue.setFooter(bot.user.username, bot.user.displayAvatarURL());
-            currentqueue.setTimestamp();
-            message.channel.send(currentqueue)
+            let embed = new Discord.MessageEmbed()
+            embed.setTitle(`Queue for: ${message.guild.name}`);
+            embed.setColor("#FFFF00");
+            embed.setFooter(bot.user.username, bot.user.displayAvatarURL());
+            embed.setTimestamp();
+
+            let counter = 0;
+            for (let i = 0; i < queue.songs.length; i += 20) {
+                if (counter >= 10) break;
+                let k = queue.songs;
+                let songs = k.slice(i, i + 20);
+                message.channel.send(embed.setDescription(songs.map((song, index) => `**${index + 1 + counter * 20}** [${song.name}](${song.url}) - ${song.formattedDuration}`)))
+                counter++;
+            }
+            //const currentqueue = new Discord.MessageEmbed();
+            //currentqueue.setTitle("Current Queue!");
+            //currentqueue.setDescription(curqueue);
+            //currentqueue.setColor("#FFFF00");
+            //currentqueue.setFooter(bot.user.username, bot.user.displayAvatarURL());
+            //currentqueue.setTimestamp();
+            //message.channel.send(currentqueue)
         } else if (!queue) {
             return message.channel.send("Nothing is playing right now!")
         };
