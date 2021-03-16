32f41213b0b6c855d946ddc895f5f6b40ba8bc89
Souvick009 <67007403+Souvick009@users.noreply.github.com>
2021-03-16 15:37:19 +0530

    Update nowplaying.js

--- commands/Music/nowplaying.js
+++ commands/Music/nowplaying.js
@@ -35,7 +35,7 @@ module.exports = {
         };
 
         //function for creating a bar
-        const createBar = function (maxtime, currenttime, size = 25, line = "▬", slider = "🔶") {
+        let createBar = function (maxtime, currenttime, size = 25, line = "▬", slider = "🔶") {
             let bar = currenttime > maxtime ? [line.repeat(size / 2 * 2), (currenttime / maxtime) * 100] : [line.repeat(Math.round(size / 2 * (currenttime / maxtime))).replace(/.$/, slider) + line.repeat(size - Math.round(size * (currenttime / maxtime)) + 1), currenttime / maxtime];
             if (!String(bar).includes("🔶")) return `**[🔶${line.repeat(size - 1)}]**\n**00:00:00 / 00:00:00**`;
             return `**[${bar[0]}]**\n**${new Date(currenttime).toISOString().substr(11, 8) + " / " + (maxtime == 0 ? " ◉ LIVE" : new Date(maxtime).toISOString().substr(11, 8))}**`;
