660d24df137540a1812ab47a39963ac3d0592c98
Souvick009 <67007403+Souvick009@users.noreply.github.com>
2021-03-17 14:18:34 +0530

    Update nowplaying.js

--- commands/Music/nowplaying.js
+++ commands/Music/nowplaying.js
@@ -49,8 +49,8 @@ module.exports = {
             let track = queue.songs[0];
 
             // assaign values to total and current
-            var total = track.duration;
-            var current = queue.currentTime;
+            var total = track.formattedDuration;
+            var current = queue.formattedCurrentTime;
             const size = 15;
             const line = "▬";
             const slider = "🔶";
