45b8090cd8b43d5d47b4b5a4e205c7152010f056
Souvick009 <67007403+Souvick009@users.noreply.github.com>
2021-04-19 13:31:07 +0530

    Update nowplaying.js

--- commands/Music/nowplaying.js
+++ commands/Music/nowplaying.js
@@ -58,7 +58,7 @@ module.exports = {
             console.log(newTotal)
             var totalperct = (newCurrent/newTotal)*100
             console.log(totalperct)
-            let bar = createBar(100, totalperct, 25);
+            let bar = createBar(100, totalperct, 25); //createBar(total, current, size, line, slider);
             console.log(bar)
             // Call the createBar method, first two arguments are mandatory
             // size (length of bar) default to 40, line default to '▬' and slider default to 🔘
