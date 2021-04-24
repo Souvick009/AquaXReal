37cc6afdddbde1801c4897c259b509bc4aed27be
Souvick009 <67007403+Souvick009@users.noreply.github.com>
2021-04-24 12:32:29 +0530

    Update prefix.js

--- models/prefix.js
+++ models/prefix.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const prefixSchema = new mongoose.Schema({
     serverId: { type: String },
-    prefix: {type: String, default: ">"}
+    prefix: {type: String, default: ">>"}
 })
 
 module.exports = mongoose.model("Prefix", prefixSchema)
