cece6daf51470d45683ccbeaea959e5bee0af8ad
Souvick009 <67007403+Souvick009@users.noreply.github.com>
2021-04-24 13:10:54 +0530

    Update set-prefix.js

--- commands/Info/set-prefix.js
+++ commands/Info/set-prefix.js
@@ -12,28 +12,35 @@ module.exports = {
   category: "Info",
   run: async (bot, message, args) => {
 
-    if(!args[0]) return message.reply("Please provide a argument to set prefix!")
-    Prefix.findOne({
-        serverId: message.guild.id,
-    }, async (err, server) => {
-        if (err) console.log(err);
-        if (!server) {
-            const newserver = new Prefix({
-                serverId: message.guild.id,
-                prefix: args.join(" "),
-            })
-            await newserver.save().catch(e => console.log(e));
-        } else if(server){
-          server.prefix = args.join(" ")
-          await server.save().catch(e => console.log(e));
-        }
+    if (!message.member.hasPermission("ADMINSTRATOR")) {
+      const embed = new Discord.MessageEmbed()
+      embed.setColor(0xFF0000)
+      embed.setDescription("❌ You don't have permissions to clear warns. Please contact a staff member.[Missing Permission:- Administrator]")
+      return message.channel.send(embed);
+  }
 
-        message.channel.send(`My new prefix is now **\`${args.join(" ")}\`**`);
-        
+    if (!args[0]) return message.reply("Please provide a argument to set prefix!")
+    Prefix.findOne({
+      serverId: message.guild.id,
+    }, async (err, server) => {
+      if (err) console.log(err);
+      if (!server) {
+        const newserver = new Prefix({
+          serverId: message.guild.id,
+          prefix: args.join(" "),
+        })
+        await newserver.save().catch(e => console.log(e));
+      } else if (server) {
+        server.prefix = args.join(" ")
+        await server.save().catch(e => console.log(e));
+      }
+
+      message.channel.send(`My new prefix is now **\`${args.join(" ")}\`**`);
+
 
     })
-   
-    
+
+
 
   }
 
