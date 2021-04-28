f61ef96977b35e711c6b18bc986c1ea5f746ee5c
Souvick009 <67007403+Souvick009@users.noreply.github.com>
2021-04-28 12:27:32 +0530

    Update play.js

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -3,6 +3,12 @@ const prefix = '>>';
 const ytdl = require('ytdl-core');
 const ytSearch = require('yt-search');
 const { getTracks, getPreview } = require("spotify-url-info");
+var SpotifyWebApi = require("spotify-web-api-node")
+var spotifyApi = new SpotifyWebApi({
+    clientId: '3a80dcbbc9ed40fa98dbbdcf09c4e1c3',
+    clientSecret: 'ba8021e08dbb4251817fcec09b8b175b',
+    redirectUri: 'http://www.example.com/callback/'
+});
 
 module.exports = {
     name: "play",
@@ -52,23 +58,60 @@ module.exports = {
         search.setColor("#FFFF00");
         message.channel.send(search)
 
+        spotifyApi.setRefreshToken("AQDV65rh2r_vOR6SZcJVX1l4hnLz6VoIbULhT1BddklkIUSTSKAmF2DLAO-ok3muVKmkjOP-Ppb1GJOzL024TJD-2fXUcwt7zvgZTyVZtMIqLARN6-h9Z30eAChCLw2U1vQ");
+
+        spotifyApi.refreshAccessToken().then(
+            function (data) {
+                console.log('The access token has been refreshed!');
+                console.log(data.body['access_token'])
+                // Save the access token so that it's used in future calls
+                spotifyApi.setAccessToken(data.body['access_token']);
+            },
+            function (err) {
+                console.log('Could not refresh access token', err);
+            }
+        );
+
+        var link = args.join(" ")
+        var split = link.split("/")
+        var id = split[4].split("?")[0]
+        console.log(id)
+
+        async function getStuff() {
+            await spotifyApi.getPlaylist(id)
+                .then(function (data) {
+                    const embed = new Discord.MessageEmbed();
+                    embed.setTitle("ADDED A SPOTIFY PLAYLIST TO QUEUE!");
+                    embed.setColor("#FFFF00");
+                    embed.setDescription(data.body["name"]);
+                    embed.addField(`Playlist Owner`, data.body.owner.display_name)
+                    embed.addField(`Total Tracks`, data.body.tracks.total)
+                    embed.setFooter(bot.user.username, bot.user.displayAvatarURL());
+                    embed.setTimestamp();
+                    message.channel.send(embed)
+                }, function (err) {
+                    console.log('Something went wrong!', err);
+                });
+        }
+
         //https://open.spotify.com/track/5nTtCOCds6I0PHMNtqelas
         if (args.join(" ").toLowerCase().includes("spotify") && args.join(" ").toLowerCase().includes("track")) {
             getPreview(args.join(" ")).then(result => {
                 bot.distube.play(message, result.title);
+                message.channel.send(`Added ${result.songl} to the queue`)
             })
         }
         else if (args.join(" ").toLowerCase().includes("spotify") && args.join(" ").toLowerCase().includes("playlist")) {
             getTracks(args.join(" ")).then(result => {
                 for (const song of result)
                     bot.distube.play(message, song.name);
+                getStuff()
             })
         }
         else {
             const music = args.join(" ");
-
-
             bot.distube.play(message, music)
+            message.channel.send(`Added ${music} to the queue`)
         }
     }
 
