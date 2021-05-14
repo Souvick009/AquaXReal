eb518e9d9c7260ab3140335555a0eefab65f8f72
Souvick009 <67007403+Souvick009@users.noreply.github.com>
2021-05-15 04:58:18 +0530

    Update nowplaying.js

--- commands/Music/nowplaying.js
+++ commands/Music/nowplaying.js
@@ -58,7 +58,7 @@ module.exports = {
             console.log(newTotal)
             var totalperct = (newCurrent/newTotal)*100
             console.log(totalperct)
-            let bar = progressbar.splitBar(100, totalperct, 25); //createBar(total, current, size, line, slider);
+            let bar = progressbar.filledBar(100, totalperct, 25); //createBar(total, current, size, line, slider);
             console.log(bar)
             // Call the createBar method, first two arguments are mandatory
             // size (length of bar) default to 40, line default to '▬' and slider default to 🔘
