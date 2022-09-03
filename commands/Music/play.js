c21055ab9e0c5a2e4e02d805c2ba9511a50e6320
Souvick <souvick8670527410@gmail.com>
2022-09-03 14:50:34 +0530

    initials

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -10,6 +10,13 @@ module.exports = {
     example: "/play ",
     cooldown: 5,
     category: "Music",
+    options: [{
+        name: "user",
+        description: "For which user avatar should be sent? Defaults to author",
+        required: false,
+        type: 6, //https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-structure
+        req: "user"
+    }],
     run: async (bot, message, args, options, author) => {
         //Checking for the voicechannel and permissions (you can add more permissions if you like).
         const voice_channel = author.voice.channel;
@@ -35,33 +42,12 @@ module.exports = {
             return send(message, { embeds: [samevc] })
         };
 
-        const Searchterm = new Discord.MessageEmbed()
-        if (!args[0]) {
-            Searchterm.setColor("#FF0000")
-            Searchterm.setFooter({ text: bot.user.username, iconURL: bot.user.displayAvatarURL() })
-            Searchterm.setTitle(`❌ ERROR | You didn't provided a Searchterm`)
-            Searchterm.setDescription(`Usage: \`/play <URL / TITLE>\``)
-            return send(message, { embeds: [Searchterm] })
-        };
         const search = new Discord.MessageEmbed()
-        search.setDescription(":mag: **Searching! **" + args.join(" "))
+        search.setDescription(":mag: **Searching! **" + options[0])
         search.setColor("#FFFF00");
         send(message, { embeds: [search] })
 
-        // //https://open.spotify.com/track/5nTtCOCds6I0PHMNtqelas
-        // if (args.join(" ").toLowerCase().includes("spotify") && args.join(" ").toLowerCase().includes("track")) {
-        //     getPreview(args.join(" ")).then(result => {
-        //         bot.distube.play(message, result.title);
-        //     })
-        // }
-        // else if (args.join(" ").toLowerCase().includes("spotify") && args.join(" ").toLowerCase().includes("playlist")) {
-        //     getTracks(args.join(" ")).then(result => {
-        //         for (const song of result)
-        //             bot.distube.play(message, song.name);
-        //     })
-        // }
-        // else {
-        const music = args.join(" ");
+        const music = options[0];
         bot.distube.play(author.voice.channel, music, {
             member: author,
             textChannel: message.channel,
