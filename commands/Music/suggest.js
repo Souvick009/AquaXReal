1ca1f9f78fc71de78740471ab04efcd462986f38
Souvick <souvick8670527410@gmail.com>
2022-09-06 20:40:37 +0530

    initials

---
+++ commands/Music/suggest.js
@@ -0,0 +1,76 @@
+const Discord = require("discord.js");
+const send = require("../../utils/sendMessage.js")
+const getMember = require("../../utils/getMember.js");
+
+module.exports = {
+    name: "suggest",
+    accessableby: "Everyone",
+    description: "Suggest the currently playing song to the mentioned user",
+    usage: "/suggest",
+    example: "/suggest",
+    cooldown: 5,
+    category: "Music",
+    options: [{
+        name: "User",
+        description: "The user to whom you want to suggest",
+        required: true,
+        type: 6, //https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-structure
+        req: "user"
+    }],
+    run: async (bot, message, args, options, author) => {
+        const voice_channel = message.member.voice.channel;
+        const vc = new Discord.MessageEmbed()
+        if (!voice_channel) {
+            vc.setColor("#FF0000")
+            vc.setTitle(`❌ ERROR | Please join a voice channel first`)
+            return send(message, { embeds: [vc] })
+        };
+
+        let channel = message.member.voice.channel.id;
+        const samevc = new Discord.MessageEmbed()
+        if (bot.distube.getQueue(message) && channel !== message.guild.me.voice.channel.id) {
+            samevc.setColor("#FF0000")
+            samevc.setTitle(`❌ ERROR | Please join **my** voice channel first`)
+            samevc.setDescription(`Channelname: \`${message.guild.me.voice.channel.name}\``)
+            return send(message, { embeds: [samevc] })
+        };
+
+
+        let queue = bot.distube.getQueue(message);
+        if (queue) {
+            // var mentionedUser = options[0]
+            var mentionedUser = await getMember(bot, args, options, message, author, false, true, 0, false)
+            const dmEmbed = new Discord.MessageEmbed()
+                .setColor(0x00FFFF)
+                .setThumbnail(queue.songs[0].thumbnail)
+                .setDescription(`[${queue.songs[0].name}](${queue.songs[0].url}) -  Suggested by ${mentionedUser}`)
+                .setTimestamp()
+            var blocked = false;
+            await mentionedUser.send({
+                embeds: [dmEmbed],
+            }).catch(error => {
+                if (error.code === 50007) {
+                    blocked = true;
+                } else {
+                    console.log(error)
+                }
+            }).finally(async () => {
+                if (blocked) {
+                    const errEmbed = new Discord.MessageEmbed();
+                    errEmbed.setColor(0xFF0000)
+                    errEmbed.setDescription(`❌ The user's dm is closed! `);
+                    return send(message, {
+                        embeds: [errEmbed]
+                    }, false);
+                } else {
+                    const save = new Discord.MessageEmbed()
+                    save.setColor("#00ff00");
+                    save.setDescription(`✅ Sent the name of the current track in your dms`)
+                    return send(message, { embeds: [save] })
+                }
+            })
+        } else if (!queue) {
+            return send(message, { content: "Nothing is playing right now!" })
+        };
+    }
+}
