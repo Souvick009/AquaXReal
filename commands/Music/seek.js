f6a10e676833fa6314fe8fc8b5e0a4b39bf37ea9
Souvick <souvick8670527410@gmail.com>
2022-10-23 12:07:49 +0530

    Fixed seek cmd

--- commands/Music/seek.js
+++ commands/Music/seek.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const send = require("../../utils/sendMessage.js")
+const ms = require("ms");
 
 module.exports = {
     name: "seek",
@@ -14,7 +15,7 @@ module.exports = {
         name: "time",
         description: "the timestamp on which you want to seek the song",
         required: true,
-        type: 4, //https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-structure
+        type: 3, //https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-type
         req: "user"
     }],
     run: async (bot, message, args, options, author) => {
@@ -46,19 +47,17 @@ module.exports = {
         var total;
         if (total2.includes(`:`)) {
             let total1 = total2.split(`:`)
-            console.log(total1)
             if (total1.length == 2) { // 1:30  // Minutes to Secs
                 let min = total1[0] * 60 // 60 sec
                 let sec = total1[1] // 30
-                total = min + sec
-                console.log(total)
+                total = parseInt(min) + parseInt(sec)
             } else if (total1.length == 3) { // 1:20:30  //Hours to Secs
                 let hou = total1[0] * 60 * 60 // 3600 sec
                 let min = total1[1] * 60 // 60 sec
                 let sec = total1[2] // 30
-                total = hou + min + sec
+                total = parseInt(hou) + parseInt(min) + parseInt(sec)
             } else {
-                total = total1
+                total = parseInt(total1)
             }
         }
         // console.log(`total ` + total)
@@ -66,43 +65,50 @@ module.exports = {
         let input2 = options[0]
         if (input2.includes(`:`)) {
             let input = input2.split(`:`)
-            console.log(input)
             if (input.length >= 2) { // 1:30  // Minutes to Secs
                 let min = input[0] * 60 // 60 sec
                 let sec = input[1] // 30
-                time = min + sec
-                console.log(time)
+                time = parseInt(min) + parseInt(sec)
             } else if (input.length >= 3) { // 1:20:30  //Hours to Secs
                 let hou = input[0] * 60 * 60 // 3600 sec
                 let min = input[1] * 60 // 60 sec
                 let sec = input[2] // 30
-                time = hou + min + sec
+                time = parseInt(hou) + parseInt(min) + parseInt(sec)
             } else {
-                time = input
+                time = parseInt(input)
             }
+        } else if (input2.includes('m')) {
+            let input = input2.split(`m`)
+            let min = input[0] * 60 // 60 sec
+            time = parseInt(min)
+        } else if (input2.includes('h')) {
+            let input = input2.split(`m`)
+            let hour = input[0] * 60 * 60 // 60 sec
+            time = parseInt(hour)
+        } else {
+            time = parseInt(input2)
         }
-        // console.log(time)
 
-        // const embed1 = new Discord.MessageEmbed()
-        // if (time < 0 || time > total) {
-        //     embed1.setColor("#FF0000")
-        //     embed1.setFooter(bot.user.username, bot.user.displayAvatarURL())
-        //     embed1.setTitle(`❌ ERROR | Seeking out of Range`)
-        //     return message.channel.send(embed1)
-        // }
+        const embed1 = new Discord.MessageEmbed()
+        // console.log(`Time : ` + time + `\nTotal : ` + total)
+        if (time < 0 || time > total) {
+            embed1.setColor("#FF0000")
+            embed1.setDescription(`❌ ERROR | Seeking out of Range`)
+            return send(message, { embeds: [embed1] })
+        }
 
         const alreadyPaused = new Discord.MessageEmbed()
-        if (bot.distube.isPaused(message)) {
+        if (queue.paused) {
             alreadyPaused.setColor("#FF0000");
             alreadyPaused.setTitle(`❌ ERROR | Cannot seek the Song`);
             alreadyPaused.setDescription(`First resume the song then try to seek!`);
             return send(message, { embeds: [alreadyPaused] })
         };
 
-        bot.distube.seek(message, Number(time * 1000));
+        bot.distube.seek(message, Number(time));
         const seek = new Discord.MessageEmbed()
         seek.setTitle(":fast_forward: Seeked!");
-        seek.setDescription(`Seeked the song for \`${time}\``)
+        seek.setDescription(`Seeked the song for \`${ms((time * 1000), { long: true })}\``)
         seek.setColor("#00ff00");
         return send(message, { embeds: [seek] })
 
